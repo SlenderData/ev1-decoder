52b043cc4b233e0511ba5f4383d1cb0112527355
SlenderData <mayunji2001@outlook.com>
2025-12-24 23:22:14 +0800

    chore: adapt project for macOS

--- ev1-dec.py
+++ ev1-dec.py
@@ -1,37 +1,42 @@
-import os, tkinter, windnd, chardet, locale
+import os
+import locale
+import tkinter as tk
+from tkinterdnd2 import DND_FILES, TkinterDnD
 
-def dnd_file(files):
+def dnd_file(event):
+    files = root.tk.splitlist(event.data)
     for file in files:
-        try:
-            file = file.decode(sys_enc)
-        except UnicodeDecodeError:
-            file = file.decode('utf-8')
+        file = file.strip('{}')  # mac 拖拽路径可能带 {}
 
-        if (file.split(".")[-1] != "ev1"):
+        if not file.lower().endswith('.ev1'):
             print(f"Ignore {file}")
             continue
 
         print(f"Convert {file}")
+
         with open(file, 'rb+') as f:
             raw = f.read(100)
             data = bytearray(raw)
 
-            for idx, b in enumerate(data):
-                data[idx] = b ^ 0xff
-            
-            raw = bytes(data)
+            for i in range(len(data)):
+                data[i] ^= 0xff
+
             f.seek(0)
-            f.write(raw)
-            f.close()
+            f.write(data)
 
-            os.rename(file, file + '.flv')
+        os.rename(file, file + '.flv')
 
+# 编码（mac 这里基本用不到 decode 了）
 sys_enc = locale.getpreferredencoding()
 
-root = tkinter.Tk()
+root = TkinterDnD.Tk()
 root.geometry('400x300')
-root.title('DV1: EV1 Decoder')
-windnd.hook_dropfiles(root, func=dnd_file)
-label = tkinter.Label(root, text ='Drop *.ev1 on me :)')
-label.place(relx = 0.5, rely = 0.5, anchor = 'center')
+root.title('DV1: EV1 Decoder (mac)')
+
+label = tk.Label(root, text='Drop *.ev1 on me :)', font=('Arial', 14))
+label.pack(expand=True)
+
+label.drop_target_register(DND_FILES)
+label.dnd_bind('<<Drop>>', dnd_file)
+
 root.mainloop()
